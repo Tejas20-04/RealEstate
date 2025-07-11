2e322329644e3f6f02c0dc02beecbfbda299f5e8
Tejas20-04 <tejasaher25@gmail.com>
2025-07-11 22:55:03 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import React from "react";
-import Header from "./components/header";
+import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
